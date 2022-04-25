e775704bbc654343eaf72313cc14526a640d74c1
Paul <paul_stephane_lucas@yahoo.fr>
2022-04-25 19:37:52 +0200

    cabug

--- php/addclient.php
+++ php/addclient.php
@@ -7,7 +7,7 @@ include('connexion.inc.php');
 $requete=$conn->prepare("INSERT INTO clients VALUES (DEFAULT,?,?,?,?,?,?,?,?,?,?,?)");    
 $requete->execute(array($_POST['nom'],$_POST['prenom'],$_POST['date'],$_POST['adresse'],$_POST['tel'],$_POST['niveau_ski'],$_POST['taille'],$_POST['poids'],$_POST['pointure'],'aucun',$_POST['formule'])); // mettre la liste des éléments qui remplaceront les ?
 print_r($requete->errorInfo());
-$code_client = $requete->fetch();
+$code_client = $requete->fetch(PDO::FETCH_OBJ);
 print_r($code_client);
 echo '<br>';
 
@@ -18,6 +18,7 @@ echo '<br>';
 $requete2=$conn->prepare("INSERT INTO login VALUES (?,?,?)");  
 
 $requete2->execute(array($_POST['id'],md5($_POST['psw']),$code_client)); // mettre la liste des éléments qui remplaceront les ?
+echo $conn->lastInsertId();
 print_r($requete2->errorInfo());
 
 
--- php/editInfo.php
+++
@@ -1,5 +0,0 @@
-<?php
-
-// faire une requete pour changer les infos d'un client
-
-?>
