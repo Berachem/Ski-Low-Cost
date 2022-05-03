afd3dec32d3d171213a3d7f4157ca10c71f0be96
Paul <paul_stephane_lucas@yahoo.fr>
2022-05-03 18:54:47 +0200

    calcule prix groupe

--- php/affectchambre.php
+++ php/affectchambre.php
@@ -6,7 +6,7 @@ session_start();
 $numgroupe = "SELECT numg FROM appartient WHERE codec = ".$_SESSION['code'];
 $membres = $conn->query("SELECT nomc,prenomc,clients.codec FROM clients,appartient WHERE appartient.codec = clients.codec and appartient.numg IN (".$numgroupe.") ORDER BY nomc,prenomc");
 
-
+//affecte les chambres aux personnes du groupe
 $index = 0;
 while( $ligne = $membres->fetch(PDO::FETCH_OBJ) ){
     $verif = $conn->query("SELECT * FROM occupe WHERE codec = ".$ligne->codec);
@@ -22,11 +22,36 @@ while( $ligne = $membres->fetch(PDO::FETCH_OBJ) ){
 }
 
 
+$gens = $conn->query("SELECT formulec,date_de_naissancec AS d FROM clients,appartient WHERE clients.codec = appartient.codec AND appartient.numg = 'G".$_SESSION['code']."'");
+$total = 0;
+
+//calcule le prix total du groupe
+while( $ligne = $gens->fetch(PDO::FETCH_OBJ) ){
+    $compt = $conn->query("SELECT prix FROM formule WHERE formulec = '".$ligne->formulec."'");
+
+    while($ligne2 = $compt->fetch(PDO::FETCH_OBJ)){
+        $today = date("Y-m-d");
+        $diff = date_diff(date_create($ligne->d), date_create($today));
+        if (2 > $diff->format('%y')){
+            $total += 0;
+        }elseif (12 > $diff->format('%y')){
+            $total += ($ligne2->prix)*0.8;
+        }else{
+            $total += $ligne2->prix;
+        }
+    }
+}  
+
+$existe = $conn->query("SELECT * FROM facture_groupe WHERE numg = 'G".$_SESSION['code']."'");
 
 
+if (empty($test = $existe->fetch(PDO::FETCH_OBJ))){
+    $requete=$conn->exec("INSERT INTO facture_groupe VALUES ('FactGroup".$_SESSION['code']."','".$total."','G".$_SESSION['code']."')");
+}else{
+    $requete=$conn->exec("UPDATE facture_groupe SET montantfacture = ".$total." WHERE numg ='G".$_SESSION['code']."'");
+}
 
-
-header('Location: ../myaccount.php');
+header('Location: ../group.php');
 
 
 ?>
